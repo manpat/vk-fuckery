e28f8a7e7f5267556c2c999086c9acb4840e6a2a
Patrick Monaghan <0x5f.manpat@gmail.com>
2024-11-21 23:08:49 +0000

    split image and rt requirements to avoid overaligning regular images

--- src/gfx/core.rs
+++ src/gfx/core.rs
@@ -240,6 +240,10 @@ pub struct DeviceAllocator {
 
 	device_local_memory_type_index: u32,
 	device_local_memory_heap_budget: u64,
+
+	buffer_alignment: u64,
+	image_alignment: u64,
+	rt_alignment: u64,
 }
 
 impl DeviceAllocator {
@@ -252,7 +256,6 @@ impl DeviceAllocator {
 		// Just everything we could possibly want from a buffer
 		let buffer_usage = vk::BufferUsageFlags::TRANSFER_SRC
 			| vk::BufferUsageFlags::TRANSFER_DST
-			| vk::BufferUsageFlags::UNIFORM_BUFFER
 			| vk::BufferUsageFlags::STORAGE_BUFFER
 			| vk::BufferUsageFlags::INDEX_BUFFER
 			| vk::BufferUsageFlags::INDIRECT_BUFFER
@@ -270,14 +273,14 @@ impl DeviceAllocator {
 		let image_usage = vk::ImageUsageFlags::TRANSFER_SRC
 			| vk::ImageUsageFlags::TRANSFER_DST
 			| vk::ImageUsageFlags::SAMPLED
-			| vk::ImageUsageFlags::STORAGE
-			| vk::ImageUsageFlags::COLOR_ATTACHMENT
-			| vk::ImageUsageFlags::DEPTH_STENCIL_ATTACHMENT
-			| vk::ImageUsageFlags::TRANSIENT_ATTACHMENT;
+			| vk::ImageUsageFlags::STORAGE;
+
+		let rt_usage = vk::ImageUsageFlags::COLOR_ATTACHMENT
+			| vk::ImageUsageFlags::DEPTH_STENCIL_ATTACHMENT;
 
 		let image_create_info = vk::ImageCreateInfo::default()
 			.image_type(vk::ImageType::TYPE_2D)
-			.format(vk::Format::R8_UNORM)
+			.format(vk::Format::R8G8B8A8_UNORM)
 			.samples(vk::SampleCountFlags::TYPE_1)
 			.extent(vk::Extent3D{ width: 64, height: 64, depth: 1 })
 			.mip_levels(1)
@@ -289,8 +292,15 @@ impl DeviceAllocator {
 		let image_requirements_query = vk::DeviceImageMemoryRequirements::default()
 			.create_info(&image_create_info);
 
+		let rt_create_info = image_create_info.clone()
+			.usage(rt_usage);
+
+		let rt_requirements_query = vk::DeviceImageMemoryRequirements::default()
+			.create_info(&rt_create_info);
+
 		let mut buffer_requirements = vk::MemoryRequirements2::default();
 		let mut image_requirements = vk::MemoryRequirements2::default();
+		let mut rt_requirements = vk::MemoryRequirements2::default();
 
 		// TODO(pat.m): max allocation count
 
@@ -298,10 +308,12 @@ impl DeviceAllocator {
 			core.vk_instance.get_physical_device_memory_properties2(core.vk_physical_device, &mut memory_props);
 			core.vk_device.get_device_buffer_memory_requirements(&buffer_requirements_query, &mut buffer_requirements);
 			core.vk_device.get_device_image_memory_requirements(&image_requirements_query, &mut image_requirements);
+			core.vk_device.get_device_image_memory_requirements(&rt_requirements_query, &mut rt_requirements);
 		};
 
 		let buffer_requirements = buffer_requirements.memory_requirements;
 		let image_requirements = image_requirements.memory_requirements;
+		let rt_requirements = rt_requirements.memory_requirements;
 
 		let memory_props = memory_props.memory_properties;
 
@@ -329,6 +341,9 @@ impl DeviceAllocator {
 
 		log::info!("Allowed image memory types: 0b{:b}", image_requirements.memory_type_bits);
 		log::info!("Image alignment requirement: {}", image_requirements.alignment);
+
+		log::info!("Allowed rendertarget memory types: 0b{:b}", rt_requirements.memory_type_bits);
+		log::info!("Rendertarget alignment requirement: {}", rt_requirements.alignment);
 
 		// Select the host local memory type with the largest associated heap for staging memory
 		let (staging_memory_type_index, selected_memory_type) = memory_props.memory_types.iter().enumerate()
@@ -366,7 +381,8 @@ impl DeviceAllocator {
 				let has_desired_flags = memory_type.property_flags.contains(vk::MemoryPropertyFlags::DEVICE_LOCAL);
 				let allows_buffers = (1 << index) & buffer_requirements.memory_type_bits != 0;
 				let allows_images = (1 << index) & image_requirements.memory_type_bits != 0;
-				allows_buffers && allows_images && has_desired_flags
+				let allows_rts = (1 << index) & rt_requirements.memory_type_bits != 0;
+				allows_buffers && allows_images && allows_rts && has_desired_flags
 			})
 			.max_by_key(|(_, memory_type)| memory_budgets.heap_budget[memory_type.heap_index as usize])
 			.context("Couldn't find device local memory type")?;
@@ -387,6 +403,10 @@ impl DeviceAllocator {
 			// TODO(pat.m): maybe storing these doesn't make sense?
 			staging_memory_heap_budget,
 			device_local_memory_heap_budget,
+
+			buffer_alignment: buffer_requirements.alignment,
+			image_alignment: image_requirements.alignment,
+			rt_alignment: rt_requirements.alignment,
 		})
 	}
 
